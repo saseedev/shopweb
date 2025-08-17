402a3a5f37956d7b974468ba0a0f4c883fbc9121
Saseendaharn <saseendharan760@gmail.com>
2025-08-17 12:14:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var sidenav=document.querySelector('.side-nav')
+
+
+function shownavbar()
+{
+  sidenav.style.left="0"
+}
+
+function closenavbar()
+{
+    sidenav.style.left="-60%"
+
+}
+
+var productContainer=document.getElementById("product")
+var search=document.getElementById("search")
+var productlist=productContainer.querySelectorAll('div')
+search.addEventListener("keyup",function(){
+  var enteredValue=event.target.value.toUpperCase()
+
+  for(count=0;count<productlist.length; count++){
+
+    var productname=productlist[count].querySelector('p').textContent
+
+    if(productname.toUpperCase().indexOf(enteredValue)<0){
+      productlist[count].style.display="none"
+    }
+    else{
+      productlist[count].style.display="block"
+    }
+
+  }
+})
